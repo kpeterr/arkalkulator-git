a4fd2c0d962939c84601053c71321cf60dcaa188
kpeterr <kpeterr2@gmail.com>
2017-04-08 21:52:45 +0200

    Kezdeti lépések

---
+++ js/fuggvenyek.js
@@ -0,0 +1,37 @@
+////////// ÁLLANDÓK //////////
+var afa = 1.27;
+
+////////// DIGITÁLIS NYOMTATÁS //////////
+
+// Méretek definiálása:
+var meretA3 = document.getElementById("meretA3");
+var meretA4 = document.getElementById("meretA4");
+var meretA5 = document.getElementById("meretA5");
+var meretA6 = document.getElementById("meretA6");
+var meretLA4 = document.getElementById("meretLA4");
+
+// Színek definiálása:
+var szin1 = document.getElementById("szin1"); // 1+0
+var szin2 = document.getElementById("szin2"); // 1+1
+var szin3 = document.getElementById("szin3"); // 4+0
+var szin4 = document.getElementById("szin4"); // 4+1
+var szin5 = document.getElementById("szin5"); // 4+4
+
+
+
+var mennyiseg = parseInt(mennyiseg.value);
+var netto = document.getElementById("nyomtatas_netto");
+var brutto = document.getElementById("nyomtatas_brutto");
+colsole.log(mennyiseg);
+
+function szamitas() {
+	'use strict';
+	netto.innerHTML = szin.a3.egyPluszNulla.db1 * mennyiseg;
+	brutto.innerHTML = szin.a3.egyPluszNulla.db1 * mennyiseg * afa;
+}
+
+function torles() {
+	'use strict';
+	netto.innerHTML = "0.00";
+	brutto.innerHTML = "0.00";
+}
---
+++ js/papir.js
@@ -0,0 +1,32 @@
+var papir = {
+	"a3": {
+		"papir1": 0,
+		"papir2": 10,
+		"papir3": 20,
+		"papir4": 25,
+		"papir5": {
+			"db1": 63,
+			"db100": 40
+		},
+		"papir6": {
+			"db1": 63,
+			"db100": 40
+		},
+		"papir7": {
+			"db1": 63,
+			"db100": 40
+		},
+		"papir8": 0,
+		"papir9": 0,
+		"papir10": 0,
+		"papir11": 0,
+		"papir12": 0,
+		"papir13": 629.92,
+		"papir14": 629.92,
+		"papir15": 47.24,
+		"papir16": {
+			"db1": 236.22,
+			"db100": 80
+		}
+	}
+};
