347a9f302f106f3063b80ea1c0c59dbd2556e7a0
kpeterr <kpeterr2@gmail.com>
2017-04-10 13:41:36 +0200

    A papírok definiálása és pár függvény javítása

--- js/fuggvenyek.js
+++ js/fuggvenyek.js
@@ -17,8 +17,26 @@ var szin3 = document.getElementById("szin3"); // 4+0
 var szin4 = document.getElementById("szin4"); // 4+1
 var szin5 = document.getElementById("szin5"); // 4+4
 
+// Papírok definiálása:
+var papir1 = document.getElementById("papir1");
+var papir2 = document.getElementById("papir2");
+var papir3 = document.getElementById("papir3");
+var papir4 = document.getElementById("papir4");
+var papir5 = document.getElementById("papir5");
+var papir6 = document.getElementById("papir6");
+var papir7 = document.getElementById("papir7");
+var papir8 = document.getElementById("papir8");
+var papir9 = document.getElementById("papir9");
+var papir10 = document.getElementById("papir10");
+var papir11 = document.getElementById("papir11");
+var papir12 = document.getElementById("papir12");
+var papir13 = document.getElementById("papir13");
+var papir14 = document.getElementById("papir14");
+var papir15 = document.getElementById("papir15");
+var papir16 = document.getElementById("papir16");
 
 
+// Függvények:
 var mennyiseg = 100;
 var netto = document.getElementById("nyomtatas_netto");
 var brutto = document.getElementById("nyomtatas_brutto");
@@ -27,11 +45,12 @@ var brutto = document.getElementById("nyomtatas_brutto");
 function szamitas() {
 	'use strict';
 	netto.innerHTML = szin.a3.egyPluszNulla.db1.ar * mennyiseg;
-	brutto.innerHTML = netto.innerHTML * afa;
-}
+	var bruttoErtek = netto.innerHTML * afa;
+	brutto.innerHTML = Math.round(bruttoErtek * 100) / 100;
+};
 
 function torles() {
 	'use strict';
 	netto.innerHTML = "0.00";
 	brutto.innerHTML = "0.00";
-}
+};
